b9d68873c4a89ebe715082c33d77c921f30e8972
DavidJCobb <831497+DavidJCobb@users.noreply.github.com>
2019-04-09 17:35:03 +0200

    Update Subroutine00470050.h

--- ReverseEngineered/_DOCS/ActorWeightData/Subroutine00470050.h
+++ ReverseEngineered/_DOCS/ActorWeightData/Subroutine00470050.h
@@ -32,27 +32,27 @@ void ActorWeightData::Subroutine00470050(float Arg1, UInt32 Arg2) {
       esp4C = 0; // loop var; also stored in ebp
       //push ebx;
       do { // at 0x00470134
-         eax = this->bodyParts[esp4C].unk08;
+         TESModelTextureSwap* eax = this->bodyParts[esp4C].unk08;
          ebx = &this->bodyParts[esp4C] - 8 bytes;
          if (eax) {
-            ecx = this->unk548[esp4C].unk10;
-            if (ecx && ecx->unk18) {
+            NiNode* ecx = this->unk548[esp4C].unk10;
+            if (ecx && ecx->m_parent) {
                if (eax == this->unk548[esp4C].unk08) { // at 0x0047016A
-                  edx = this->bodyParts[esp4C].item;
+                  TESForm* edx = this->bodyParts[esp4C].item;
                   edi = &this->bodyParts[esp4C];
                   esi = &this->unk548[esp4C];
                   if (edx == esi->unk00) {
-                     this->unk548[esp4C].unk0C = this->bodyParts[esp4C].unk0C;
+                     esi->unk0C = edi->unk0C; // BGSTextureSet* assign
                      *edi = *esi;
-                     edx = this->bodyParts[esp4C].unk10;
+                     NiNode* edx = this->bodyParts[esp4C].unk10;
                      this->TESV_0046D570(edx, esp4C, esp30);
                      esi->Reset();
                      if (*(byte*)(0x012E5CA4)) {
-                        eax = this->bodyParts[esp4C].unk10;
+                        NiNode* eax = this->bodyParts[esp4C].unk10;
                         if (eax)
                            this->TESV_0046D0B0(eax, esp4C);
                      }
-                     auto ebx = this->bodyParts[esp4C].addon;
+                     TESObjectARMA* ebx = this->bodyParts[esp4C].addon;
                      if (!ebx)
                         continue;
                      if (ebx == edi->item)
@@ -64,7 +64,7 @@ void ActorWeightData::Subroutine00470050(float Arg1, UInt32 Arg2) {
                }
             }
          }
-         if (this->unk548[esp4C].unk10) { // at 0x00470206
+         if (this->unk548[esp4C].unk10) { // if it has a NiNode // at 0x00470206
             this->TESV_0046C8A0(&this->unk548[esp4C], 1, 0);
          } else {
             if (eax) { // at 0x0047021E
@@ -85,7 +85,7 @@ void ActorWeightData::Subroutine00470050(float Arg1, UInt32 Arg2) {
             }
          }
          // at 0x00470269
-         auto eax = this->bodyParts[esp4C].addon;
+         TESObjectARMA* eax = this->bodyParts[esp4C].addon;
          if (eax && eax != this->bodyParts[esp4C].item) { // at 0x00470273
             esp34 += eax->unk30; // float
          }
@@ -100,7 +100,9 @@ void ActorWeightData::Subroutine00470050(float Arg1, UInt32 Arg2) {
             //
          } else {
             if (esp4C == 0x29) { // at 0x00470386
-               
+               //
+               // ...
+               //
             } else {
                if (esp30) { // at 0x004703C0
                   if (eax = esp60) {
@@ -110,10 +112,10 @@ void ActorWeightData::Subroutine00470050(float Arg1, UInt32 Arg2) {
                         continue;
                   }
                }
-               edx = this->bodyParts[esp4C].unk08;
+               TESModelTextureSwap* edx = this->bodyParts[esp4C].unk08;
                if (!edx)
                   continue;
-               eax = this->bodyParts[esp4C].item;
+               TESForm* eax = this->bodyParts[esp4C].item;
                if (!eax)
                   continue;
                uint8_t al = eax->formType;
@@ -126,7 +128,7 @@ void ActorWeightData::Subroutine00470050(float Arg1, UInt32 Arg2) {
                   // ...
                   //
                }
-               eax = &edx->unk04.TESV_00A50AC0(); // StringCache::Ref::TESV_00A50AC0
+               eax = &edx->name.TESV_00A50AC0(); // StringCache::Ref::TESV_00A50AC0
                if (!eax)
                   continue;
                esi = 0;
@@ -144,15 +146,107 @@ void ActorWeightData::Subroutine00470050(float Arg1, UInt32 Arg2) {
                const char* eax = this->bodyParts[esp4C].unk08->GetModelName();
                auto eax = TESV_00AF5820(eax, &esp20, &esp74);
                if (!eax) { // at 0x004704B7
-                  
+                  esp98.TESV_0046DF40(1.0F);
+                  edi = esp60;
+                  eax = esp60->unk124;
+                  esp10 = esi;
+                  esp54 = esi;
+                  esp58 = esi;
+                  esp50 = esi;
+                  bool esp26 = false;
+                  float a;
+                  if (eax != esi) { // at 0x004704F2
+                     do {
+                        ecx = eax->unk124;
+                        if (ecx != esi)
+                           eax = ecx;
+                     } while (eax != esi);
+                     a = eax->unk12C;
+                  } else {
+                     a = esp18->GetFormWeight();
+                  }
+                  // FPU: [100.0F, a]
+                  // FPU: [100.0F, 100.0F, a]
+                  // FPU: [100.0F, a / 100.0F]
+                  float esp28 = a / 100.0F;
+                  // FPU: [100.0F]
+                  // FPU: [Arg1, 100.0F]
+                  // FPU: [0, Arg1, 100]
+                  if (0.0F ?? Arg1) { // at 0x00470543
+                     esp28 = Arg1;
+                  }
+                  ecx = ebx->unk08;
+                  double f6 = esp28 * 100.0F
+                  // FPU: [100]
+                  f5 = ecx->formID; // eax
+                  edx = ebx->formID;
+                  f2 = edx->formID; // ebp
+                  esi = edx;
+                  auto f4 = ecx->Unk_32(); // at 0x00470571
+                  auto f3 = edi->GetSex();
+                  auto f1 = esi->Unk_32(); // at 0x00470587
+                  UnkOutput004202A0(&esp210, 0x104, "%s (%08X)[%d]/%s (%08X) [%2.0f%%]", f1, f2, f3, f4, f5, f6); // at 0x0047059C
+                  ecx = esp2C;
+                  if ((ecx != *(PlayerCharacter**)0x01310588) && /*bool*/ TESV_00AF5030(&esp1F0, &ebx->unk24)) { // at 0x004705AE
+                     esi->unk00->unk1C->TESV_00AAFC00(&esp98); // call is completed at 0x00470817
+                  } else {
+                     bool al = TESV_00B06360(esp20); // at 0x004705DA
+                     if (al) {
+                        eax = (*g_TES)->TESV_00432070(esp20, &esp98);
+                        NiPointer esp54 = eax;
+                        eax = esp54;
+                        esi = esp54;
+                     } else {
+                        // at 0x00470618
+                        if (INI:General:bUseBodyMorphs && ebx->unk0C) {
+                           if (ebx->unk0C->unk2A[edi->GetSex()] > 1) { // at 0x0047063E
+                              //
+                              // ...
+                              //
+                           }
+                        }
+                        // at 0x0047080B
+                        ecx = esp20;
+                        // at 0x00470817
+                        esi = esp10 = ecx->TESV_00AAFC00(&esp98);
+                     }
+                     // at 0x00470822
+                     if (esi) {
+                        eax = esi->Unk_03();
+                     } else
+                        eax = 0;
+                     esp48.TESV_00489910(eax);
+                     esp10 = 0;
+                     eax = esp18->Unk_6F(esp30);
+                     ebp = esp48;
+                     TESV_00C6F3B0(esp48, eax); // at 0x0047085E
+                     if (ebp) {
+                        //
+                        // ...
+                        //
+                     }
+                     // at 0x00470BED
+                     esp10.TESV_007B1320();
+                     esp48.TESV_007B1320();
+                     esp58.TESV_007B1320();
+                     esp54.TESV_007B1320();
+                     esp98.TESV_00677F00();
+                     ebp = esp4C;
+                     // fall through to 0x00470C21
+                  }
+                  // at 0x00470817
+                  //
+                  // ...
+                  //
                }
-               esp20.TESV_007B1320();
+               esp20.TESV_007B1320(); // at 0x00470C21
             }
          }
       } while (++esp4C < 0x2A);
       // at 0x00470C38
-      
-      
+      //
+      // ...
+      //
    }
    //
    // clean up smart pointer esp14, and then return
