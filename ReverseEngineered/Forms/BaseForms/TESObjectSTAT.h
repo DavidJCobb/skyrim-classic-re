ecac4241ed029009b4ed5c1dd750fceca49393f4
DavidJCobb <831497+DavidJCobb@users.noreply.github.com>
2019-10-14 17:12:31 +0200

    TESObjectSTAT
    Identified the unknown struct inside of it as DNAM.

---
+++ ReverseEngineered/Forms/BaseForms/TESObjectSTAT.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "ReverseEngineered\Forms\TESForm.h"
+#include "ReverseEngineered\Forms\BaseForms\base.h"
+
+namespace RE {
+   class TESObjectSTAT : public TESBoundObject { // sizeof == 0x44
+      public:
+         enum { kTypeID = kFormType_Static };
+
+         struct DNAM { // sizeof == 0x8
+            float maxAngle; // 00
+            void* material; // 04 // form of type MATO
+         };
+
+         // Parents:
+         TESModelTextureSwap texSwap;	// 20
+
+         // Members:
+         DNAM directionalMaterial; // 3C
+   };
+};
